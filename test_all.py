333fde92fcd84f5509f397b7e1165850a8f82d27
Jeremy Neal <guffaw@gmail.com>
2026-04-03 15:17:54 -0500

    test: add root unittest entrypoint for deterministic gate

---
+++ test_all.py
@@ -0,0 +1,5 @@
+"""Aggregate unittest entrypoint so `python3 -m unittest` runs repo tests."""
+
+from tests.test_reporting_integrity import *  # noqa: F401,F403
+from tests.test_token_accounting import *  # noqa: F401,F403
+
